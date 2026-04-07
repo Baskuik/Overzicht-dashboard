91b0324d8d7245291c4e5a0e068eed7722910c64
Baskuik <97105236@st.deltion.nl>
2026-04-07 16:29:58 +0200

    Add upload routes in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UploadController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::post('/upload', [UploadController::class, 'store'])->name('upload.store');
 });
